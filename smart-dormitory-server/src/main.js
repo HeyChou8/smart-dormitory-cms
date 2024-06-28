d1e7958b2d06d42c9b330d0a969ef938ea048b04
heyChou <1666201454@qq.com>
2024-06-28 17:54:13 +0800

    修改了端口的读取

--- smart-dormitory-server/src/main.js
+++ smart-dormitory-server/src/main.js
@@ -1,6 +1,8 @@
 const app = require('./app/index')
+require('dotenv').config()
 require('./utils/handle_error')
+const port = process.env.PORT || 8001
 // 启动app
-app.listen(8001,'0.0.0.0',() => {
+app.listen(port,'0.0.0.0',() => {
     console.log('服务器启动')
 })
